0416d65859f97cd63641cc5e53a53cba41647f92
LuchoEmilio <lechica@uninorte.edu.co>
2021-10-21 19:51:54 -0500

    mensaje1

---
+++ forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from wtforms import TextField, SubmitField, PasswordField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import EqualTo, InputRequired
+
+class Login(FlaskForm):
+    usu = TextField('Usuario *', validators = [InputRequired(message='Indique el usuario')])
+    cla = PasswordField('Clave *', validators = [InputRequired(message='Indique la clave')])
+    btn = SubmitField('Login')
+
+class Registro(FlaskForm):
+    nom = TextField('Nombre *', validators = [InputRequired(message='Indique el nombre')])
+    usu = TextField('Usuario *', validators = [InputRequired(message='Indique el usuario')])
+    ema = EmailField('Email *', validators = [InputRequired(message='Indique el email')])
+    cla = PasswordField('Clave *', validators = [InputRequired(message='Indique la clave')])
+    ver = PasswordField('Verificación *', validators = [InputRequired(message='Indique la verificación'), EqualTo(cla,message='Clave y la verificación no coinciden')])
+    btn = SubmitField('Registrar')
+
+class Mensaje(FlaskForm):
+    de = TextField('De *', validators = [InputRequired(message='Indique el usuario')])
+    para = TextField('Para *', validators = [InputRequired(message='Indique el Destinatario')])
+    asunto = TextField('Asunto *', validators = [InputRequired(message='Indique el Asunto')])
+    mensaje = TextField('Mensaje *', validators = [InputRequired(message='Indique el Mensaje')])
+    btn = SubmitField('Enviar')
+
+  
